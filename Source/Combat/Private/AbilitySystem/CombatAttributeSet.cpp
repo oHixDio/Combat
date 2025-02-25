5814039bcc99c4285b46e689d81c6ac9cae25205
oHixDio <hry66H@outlook.jp>
2025-02-25 13:52:44 +0900

    Update AttributeSet :Added Stamina and Health

--- Source/Combat/Private/AbilitySystem/CombatAttributeSet.cpp
+++ Source/Combat/Private/AbilitySystem/CombatAttributeSet.cpp
@@ -3,3 +3,98 @@
 
 #include "AbilitySystem/CombatAttributeSet.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "GameplayEffectExtension.h"
+#include "GameFramework/Character.h"
+#include "Net/UnrealNetwork.h"
+
+UCombatAttributeSet::UCombatAttributeSet()
+{
+}
+
+void UCombatAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UCombatAttributeSet, Stamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UCombatAttributeSet, MaxStamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UCombatAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UCombatAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+}
+
+void UCombatAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetStaminaAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxStamina());
+	}
+}
+
+void UCombatAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	FEffectProperties Props;
+	SetEffectProperties(Data,Props);
+
+	if (Data.EvaluatedData.Attribute == GetStaminaAttribute())
+	{
+		SetStamina(FMath::Clamp(GetStamina(), 0.f, GetMaxStamina()));
+	}
+}
+
+void UCombatAttributeSet::SetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const
+{
+	Props.GameplayEffectContextHandle = Data.EffectSpec.GetContext();
+	
+	Props.SourceASC = Props.GameplayEffectContextHandle.GetOriginalInstigatorAbilitySystemComponent(); // ← このGEの原因となったASCを返却する(チェーンしている場合、本元を返却する).
+	if (IsValid(Props.SourceASC) && Props.SourceASC->AbilityActorInfo.IsValid() && Props.SourceASC->AbilityActorInfo->AvatarActor.IsValid())
+	{
+		Props.SourceAvatarActor = Props.SourceASC->AbilityActorInfo->AvatarActor.Get();
+		Props.SourceController = Props.SourceASC->AbilityActorInfo->PlayerController.Get();
+		// Controllerを取得できなければ、AvatarActor経由で取得を試みる.
+		if (Props.SourceController == nullptr && Props.SourceAvatarActor != nullptr)
+		{
+			if (const APawn* Pawn = Cast<APawn>(Props.SourceAvatarActor))
+			{
+				Props.SourceController = Pawn->GetController();
+			}
+		}
+		if (Props.SourceController)
+		{
+			Props.SourceCharacter = Props.SourceController->GetCharacter();
+		}
+	}
+
+	if (Data.Target.AbilityActorInfo.IsValid() && Data.Target.AbilityActorInfo->AvatarActor.IsValid())
+	{
+		Props.TargetAvatarActor = Data.Target.AbilityActorInfo->AvatarActor.Get();
+		Props.TargetController = Data.Target.AbilityActorInfo->PlayerController.Get();
+		Props.TargetCharacter = Cast<ACharacter>(Props.TargetAvatarActor);
+		Props.TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Props.TargetAvatarActor);
+	}
+}
+
+
+
+void UCombatAttributeSet::OnRep_Stamina(const FGameplayAttributeData& OldStamina) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCombatAttributeSet, Stamina, OldStamina);
+}
+
+void UCombatAttributeSet::OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCombatAttributeSet, MaxStamina, OldMaxStamina);
+}
+
+void UCombatAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCombatAttributeSet, Health, OldHealth);
+}
+
+void UCombatAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCombatAttributeSet, MaxHealth, OldMaxHealth);
+}
--- Source/Combat/Public/AbilitySystem/CombatAttributeSet.h
+++ Source/Combat/Public/AbilitySystem/CombatAttributeSet.h
@@ -4,7 +4,60 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "CombatAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+USTRUCT(BlueprintType)
+struct FEffectProperties
+{
+	GENERATED_BODY()
+
+	FEffectProperties(){}
+	FEffectProperties(
+		FGameplayEffectContextHandle EffectContextHandle,
+		UAbilitySystemComponent* SASC, AActor* SAA, AController* SController, ACharacter* SCharacter,
+		UAbilitySystemComponent* TASC, AActor* TAA, AController* TController, ACharacter* TCharacter)
+		: GameplayEffectContextHandle(EffectContextHandle),
+		  SourceASC(SASC), SourceAvatarActor(SAA), SourceController(SController), SourceCharacter(SCharacter),
+		  TargetASC(TASC), TargetAvatarActor(TAA), TargetController(TController), TargetCharacter(TCharacter){}
+
+	UPROPERTY()
+	FGameplayEffectContextHandle GameplayEffectContextHandle{};
+	
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> SourceASC{};
+
+	UPROPERTY()
+	TObjectPtr<AActor> SourceAvatarActor{};
+
+	UPROPERTY()
+	TObjectPtr<AController> SourceController{};
+
+	UPROPERTY()
+	TObjectPtr<ACharacter> SourceCharacter{};
+	
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> TargetASC{};
+
+	UPROPERTY()
+	TObjectPtr<AActor> TargetAvatarActor{};
+	
+	UPROPERTY()
+	TObjectPtr<AController> TargetController{};
+
+	UPROPERTY()
+	TObjectPtr<ACharacter> TargetCharacter{};
+};
+
+template<class T>
+using TStaticFuncPtr = typename TBaseStaticDelegateInstance<T, FDefaultDelegateUserPolicy>::FFuncPtr;
+
 
 /**
  * 
@@ -13,5 +66,34 @@ UCLASS()
 class COMBAT_API UCombatAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UCombatAttributeSet();
+
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+
+private:
+	void SetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const;
+
+public:
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Stamina, Category = "Combat | Attribute")
+	FGameplayAttributeData Stamina; ATTRIBUTE_ACCESSORS(UCombatAttributeSet, Stamina)
+	UFUNCTION() void OnRep_Stamina(const FGameplayAttributeData& OldStamina) const;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxStamina, Category = "Combat | Attribute")
+	FGameplayAttributeData MaxStamina; ATTRIBUTE_ACCESSORS(UCombatAttributeSet, MaxStamina)
+	UFUNCTION() void OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina) const;
 	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Combat | Attribute")
+	FGameplayAttributeData Health; ATTRIBUTE_ACCESSORS(UCombatAttributeSet, Health)
+	UFUNCTION() void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Combat | Attribute")
+	FGameplayAttributeData MaxHealth; ATTRIBUTE_ACCESSORS(UCombatAttributeSet, MaxHealth)
+	UFUNCTION() void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
 };
